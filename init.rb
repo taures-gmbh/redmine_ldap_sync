0cdac7500efa075262c0bb60fc399c628cf15d9a
aki360P <34680805+aki360P@users.noreply.github.com>
2021-06-07 23:07:27 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,12 +2,12 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_ldap_sync do
   name 'Redmine LDAP Sync'
-  author 'Ricardo Santos, Taine Woo'
-  author_url 'https://github.com/tainewoo'
+  author 'Akinori Iwasaki, DevOps-TauRes, Ricardo Santos, Taine Woo'
+  author_url 'https://github.com/aki360p'
   description 'Syncs users and groups with ldap'
-  url 'https://github.com/tainewoo/redmine_ldap_sync'
-  version '2.2.0'
-  requires_redmine :version_or_higher => '2.1.0'
+  url 'https://github.com/aki360p/redmine_ldap_sync'
+  version '2.3.0dev'
+  requires_redmine :version_or_higher => '4.0.0'
 
   settings :default => HashWithIndifferentAccess.new()
   menu :admin_menu, :ldap_sync, { :controller => 'ldap_settings', :action => 'index' }, :caption => :label_ldap_synchronization,

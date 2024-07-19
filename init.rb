038de267ef94cb063d4862e4f8648e0631abb2a5
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2024-07-19 11:39:06 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require 'redmine'
 Redmine::Plugin.register :redmine_ldap_sync do
   name 'Redmine LDAP Sync'
   author 'Ricardo Santos, Taine Woo'
-  author_url 'https://github.com/tainewoo'
+  author_url 'https://github.com/eea'
   description 'Syncs users and groups with ldap'
-  url 'https://github.com/tainewoo/redmine_ldap_sync'
-  version '2.2.0'
+  url 'https://github.com/eea/redmine_ldap_sync'
+  version '2.3.0'
   requires_redmine :version_or_higher => '2.1.0'
 
   settings :default => HashWithIndifferentAccess.new()
